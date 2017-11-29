371d691951fc52130a2455a0740b1f4e7187aeea
ufdilla <learnmore.cumi@gmail.com>
2017-11-29 09:27:52 +0700

    1st commit

---
+++ tiket.java
@@ -0,0 +1,23 @@
+public class tiket {
+    public static void main(String[] args) {
+    
+        int data[][] = 
+        {
+            {0, 800, 1100, 1300}, {800, 0, 500, 700}, {1000, 400, 0, 500}, {1200, 600, 400, 0}
+        };
+
+        int search = 0;
+    
+        for (int i = 0; i < data.length; i++) { //nested repetition
+            for (int j = 0; j < data[0].length; j++) {
+                if (i == j){
+                  continue;
+                } 
+                else
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
---
+++ tiket2.java
@@ -0,0 +1,17 @@
+/**
+ * tiket2
+ */
+public class tiket2 {
+
+    public static void main(String[] args) {
+        
+        int data[][] = { { 0, 800, 1100, 1300 }, { 800, 0, 500, 700 }, { 1000, 0 , 0, 500 }, { 1200, 600, 400, 0 } };
+
+        for (int i = 0; i < data.length; i++) { //nested repetition
+            for (int j = 0; j < data[0].length; j++) {
+                    System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
