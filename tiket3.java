b844452f20b2300da024c467fd33ab5d758aabb6
ufdilla <learnmore.cumi@gmail.com>
2017-11-30 11:03:40 +0700

    menggunakan continue 2 level dengan continue labeled

--- tiket3.java
+++ tiket3.java
@@ -2,22 +2,25 @@
 /**
  * tiket3
  */
-public class tiket3 {
+public class tiket3 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int[][] data = { { 0, 800, 1100, 1300 }, { 800, 0, 500, 700 }, { 1000, 400, 0, 0 }, { 1200, 600, 400, 0 } };
 
-        // label:
-        for (int i = 0; i < data.length; i++) { //nested repetition
-            for (int j = 0; j < data[0].length; j++) {
-                if (data[i][j] == 0)
-                {
-                    if (i == j )
-                     continue;
-                     
-                    else
-                     continue; 
-                 }
+        label2 :
+        for (int i = 0; i < data.length; i++) //nested repetition
+        { 
+            label1:
+            for (int j = 0; j < data[0].length; j++) 
+            {
+                if (i == j)
+                continue level1;
+                
+                
+                else if (data[i][j] == 0)
+                continue level2;
 
                 else
                 System.out.print(data[i][j] + " ");
